416b0819d98975e61844f1b20960fb8b9e712946
frinica <frida.nicander@chasacademy.se>
2022-10-12 16:36:22 +0200

    connect login in FE to BE API

--- src/pages/users/Login.tsx
+++ src/pages/users/Login.tsx
@@ -1,8 +1,11 @@
 import { FC, useState } from "react";
 import { Form, Field, Formik, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import { login } from "../../services/auth.service";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage: FC = () => {
+  const navigate = useNavigate();
   const initValues = { email: "", password: "" };
   const LoginSchema = Yup.object().shape({
     email: Yup.string().email().required(),
@@ -13,8 +16,18 @@ const LoginPage: FC = () => {
     <>
       <Formik
         initialValues={initValues}
-        onSubmit={(values, { setSubmitting }) => {
-          console.log(values);
+        onSubmit={async (values, { setSubmitting }) => {
+          const success = await login(values);
+          try {
+            if (success) {
+              navigate("/start");
+              window.location.reload();
+            } else {
+              alert("Error when trying to login");
+            }
+          } catch (err) {
+            alert(err);
+          }
           setSubmitting(false);
         }}
         validationSchema={LoginSchema}
--- src/services/auth.service.tsx
+++ src/services/auth.service.tsx
@@ -1,10 +1,23 @@
 import axios from "axios";
-import { IRegister } from "../models/User";
+import { ILogin, IRegister } from "../models/User";
 
 const API_URL = `${process.env.REACT_APP_API_URL}user/`;
 
+// Register a new user
 export const register = async (regData: IRegister) => {
   console.log(API_URL);
   const res = await axios.post(API_URL + "register", regData);
   return res.status === 200;
+};
+
+// Login user and set JWT
+export const login = async (loginData: ILogin) => {
+  const res = await axios.post(API_URL + "login", loginData);
+  const success = res.status === 200;
+
+  if (success) {
+    localStorage.setItem("token", res.data);
+  }
+
+  return success;
 };
