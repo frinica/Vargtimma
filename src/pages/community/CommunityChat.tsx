db846367bba595c30e9fe3939f36f513933e3d8d
frinica <frida.nicander@chasacademy.se>
2022-10-13 19:21:34 +0200

    fix functions to receive data from socket server and add messages display

--- src/pages/community/CommunityChat.tsx
+++ src/pages/community/CommunityChat.tsx
@@ -5,19 +5,53 @@ import { userData } from "../../services/auth.service";
 let socket;
 
 const CommunityChat: FC = (location: any) => {
-  const [username, setUsername] = useState("");
-  /* const [room, setRoom] = useState(""); */
+  const socket = io("http://localhost:5000");
+  const [username, setUsername] = useState<string>("");
+  const [room, setRoom] = useState<string | null>(null);
+  const [messages, setMessages] = useState<any[]>([]);
 
   const getUser = async () => {
     const currentUsername = await userData();
     setUsername(currentUsername);
   };
 
+  const getRoom = () => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const activeRoom = urlParams.get("room");
+    setRoom(activeRoom);
+  };
+
   useEffect(() => {
-    socket = io("http://localhost:5000");
     getUser();
+    getRoom();
+    if (username.length > 0 && room !== null) {
+      socket.emit("join", { username, room }, (error: any) => {
+        if (error) alert(error);
+      });
+    } else {
+      console.log("Failure to connect to socket");
+    }
+  }, [location.search]);
+
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages((messages) => [...messages, message]);
+    });
   }, []);
 
-  return <></>;
+  return (
+    <div>
+      {messages.map((val, i) => {
+        return (
+          <div key={i}>
+            {val.text}
+            <br />
+            {val.user}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 export default CommunityChat;
