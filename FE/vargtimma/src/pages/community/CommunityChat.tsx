fe35cbeb7c790a155e794d2fefb092aa9ce03881
frinica <frida.nicander@chasacademy.se>
2022-11-11 13:40:24 +0100

    add transport again

--- FE/vargtimma/src/pages/community/CommunityChat.tsx
+++ FE/vargtimma/src/pages/community/CommunityChat.tsx
@@ -14,9 +14,7 @@ const CommunityChat: FC = () => {
 
   // Connect user to the socket
   useEffect(() => {
-    socket = io(ENDPOINT, {
-      transports: ["polling"],
-    });
+    socket = io(ENDPOINT, { transports: ["websocket"] });
 
     socket.emit("join", { username, room }, (error: any) => {
       console.log("Connected to socket");
